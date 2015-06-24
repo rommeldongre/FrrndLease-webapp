f7b2d257ebd21221336a52ccb6d6462459fa6842
sanketspkl <sanketspkl@gmail.com>
2015-06-24 18:57:36 +0530

    ADDED REJECT REQUEST, REQUEST ITEM, GET REQUESTS AS PER NEW SCHEMA. DONT BOTHER ABOUT GRANT LEASE NOW.

--- src/tableOps/Requests.java
+++ src/tableOps/Requests.java
@@ -35,9 +35,19 @@ public class Requests extends Connect{
 			Delete();
 			break;
 			
-		case "deleteone" :
+		/*case "deleteone" :
 			System.out.println("DeleteOne op is selected");
 			DeleteOne();
+			break;*/
+			
+		case "edits" :
+			System.out.println("Edit s operation is selected");
+			EditS();
+			break;
+			
+		case "editone" :
+			System.out.println("Edit one operation is selected");
+			EditOne();
 			break;
 			
 		case "getnext" :
@@ -56,6 +66,28 @@ public class Requests extends Connect{
 			try {
 				token = obj.getString("token");
 				getPrevious();
+			} catch (JSONException e) {
+				res.setData(202, String.valueOf(token), "JSON Data not parsed/found(JSON Exception)");
+				e.printStackTrace();
+			}
+			break;
+			
+		case "getnextr" :
+			System.out.println("Get Next operation is selected.");
+			try {
+				token = obj.getString("token");
+				getNextR();
+			} catch (JSONException e) {
+				res.setData(202, String.valueOf(token), "JSON Data not parsed/found(JSON Exception)");
+				e.printStackTrace();
+			}
+			break;
+			
+		case "getpreviousr" :
+			System.out.println("Get Next operation is selected.");
+			try {
+				token = obj.getString("token");
+				getPreviousR();
 			} catch (JSONException e) {
 				res.setData(202, String.valueOf(token), "JSON Data not parsed/found(JSON Exception)");
 				e.printStackTrace();
@@ -145,7 +177,7 @@ public class Requests extends Connect{
 		
 	}
 	
-	private void DeleteOne() {
+	/*private void DeleteOne() {
 		itemId = rm.getItemId();
 		userId = rm.getUserId();
 		check = null;
@@ -174,6 +206,92 @@ public class Requests extends Connect{
 				stmt.setString(2, userId);
 				stmt.executeUpdate();
 				message = "operation successfull deleted request item id : "+itemId;
+				Code = 56;
+				Id = check;
+				res.setData(Code, Id, message);
+			}
+			else{
+				System.out.println("Entry not found in database!!");
+				res.setData(201, "0", "Entry not found in database!!");
+			}
+		} catch (SQLException e) {
+			res.setData(200, "0", "Couldn't create statement, or couldn't execute a query(SQL Exception)");
+			e.printStackTrace();
+		}
+	}*/
+	
+	private void EditS() {
+		itemId = rm.getItemId();
+		userId = rm.getUserId();
+		String status = "Archived";
+		check = null;
+		
+		System.out.println("inside edit method");
+		getConnection();
+		String sql = "UPDATE requests SET request_status=? WHERE request_item_id=?";			//
+		String sql2 = "SELECT * FROM requests WHERE request_item_id=?";								//
+		
+		try {
+			System.out.println("Creating Statement....");
+			PreparedStatement stmt2 = connection.prepareStatement(sql2);
+			stmt2.setString(1, itemId);
+			ResultSet rs = stmt2.executeQuery();
+			while(rs.next()) {
+				check = rs.getString("request_item_id");
+			}
+			
+			if(check != null) {
+				PreparedStatement stmt = connection.prepareStatement(sql);
+				
+				System.out.println("Statement created. Executing edit query on ..." + check);
+				stmt.setString(1, status);
+				stmt.setString(2,itemId);
+				stmt.executeUpdate();
+				message = "operation successfull edited item id : "+itemId;
+				Code = 55; /////////
+				Id = check;
+				res.setData(Code, Id, message);
+			}
+			else{
+				System.out.println("Entry not found in database!!");
+				res.setData(201, "0", "Entry not found in database!!");
+			}
+		} catch (SQLException e) {
+			res.setData(200, "0", "Couldn't create statement, or couldn't execute a query(SQL Exception)");
+			e.printStackTrace();
+		}
+	}
+	
+	private void EditOne(){
+		itemId = rm.getItemId();
+		userId = rm.getUserId();
+		String status = "Archived";
+		check = null;
+		
+		System.out.println("inside edit method");
+		getConnection();
+		String sql = "UPDATE requests SET request_status=? WHERE request_item_id=? AND request_requser_id=?";			//
+		String sql2 = "SELECT * FROM requests WHERE request_item_id=? AND request_requser_id=?";								//
+		
+		try {
+			System.out.println("Creating Statement....");
+			PreparedStatement stmt2 = connection.prepareStatement(sql2);
+			stmt2.setString(1, itemId);
+			stmt2.setString(2, userId);
+			ResultSet rs = stmt2.executeQuery();
+			while(rs.next()) {
+				check = rs.getString("request_item_id");
+			}
+			
+			if(check != null) {
+				PreparedStatement stmt = connection.prepareStatement(sql);
+				
+				System.out.println("Statement created. Executing edit query on ..." + check);
+				stmt.setString(1, status);
+				stmt.setString(2,itemId);
+				stmt.setString(3, userId);
+				stmt.executeUpdate();
+				message = "operation successfull edited item id : "+itemId;
 				Code = 56;
 				Id = check;
 				res.setData(Code, Id, message);
@@ -246,6 +364,100 @@ public class Requests extends Connect{
 			
 			System.out.println("Statement created. Executing getPrevious query...");
 			stmt.setString(1, token);
+			
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				JSONObject json = new JSONObject();
+				json.put("itemId", rs.getString("request_item_id"));
+				json.put("userId", rs.getString("request_requser_id"));
+				json.put("date", rs.getString("request_date"));
+				
+				message = json.toString();
+				System.out.println(message);
+				check = rs.getString("request_item_id");
+			}
+			
+			if(check != null ) {
+				Code = 28;
+				Id = check;
+			}
+			
+			else {
+				Id = null;
+				message = "End of Database!!!";
+				Code = 199;
+			}
+			
+			res.setData(Code,Id,message);
+		} catch (SQLException e) {
+			res.setData(200, "0", "Couldn't create statement, or couldn't execute a query(SQL Exception)");
+			e.printStackTrace();
+		} catch (JSONException e) {
+			res.setData(204,"0", "JSON Exception");
+			e.printStackTrace();
+		}	
+	}
+	
+	private void getNextR() {
+		check = null;
+		System.out.println("Inside GetNextR method");
+		String sql = "SELECT * FROM requests WHERE request_item_id > ? AND request_status=? ORDER BY request_item_id LIMIT 1";		//
+		
+		getConnection();
+		try {
+			System.out.println("Creating a statement .....");
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			
+			System.out.println("Statement created. Executing getNext query...");
+			stmt.setString(1, token);
+			stmt.setString(2, "Active");
+			
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				JSONObject json = new JSONObject();
+				json.put("itemId", rs.getString("request_item_id"));
+				json.put("userId", rs.getString("request_requser_id"));
+				json.put("date", rs.getString("request_date"));
+				
+				message = json.toString();
+				System.out.println(message);
+				check = rs.getString("request_item_id");
+			}
+			
+			if(check != null ) {
+				Code = 27;
+				Id = check;
+			}
+			
+			else {
+				Id = null;
+				message = "End of Database!!!";
+				Code = 199;
+			}
+			
+			res.setData(Code,Id,message);
+		} catch (SQLException e) {
+			res.setData(200, "0", "Couldn't create statement, or couldn't execute a query(SQL Exception)");
+			e.printStackTrace();
+		} catch (JSONException e) {
+			res.setData(204,"0", "JSON Exception");
+			e.printStackTrace();
+		}	
+	}
+	
+	private void getPreviousR() {
+		check = null;
+		System.out.println("Inside GetPrevious method");
+		String sql = "SELECT * FROM requests WHERE request_item_id < ? AND request_status=? ORDER BY request_item_id DESC LIMIT 1";			//
+		
+		getConnection();
+		try {
+			System.out.println("Creating a statement .....");
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			
+			System.out.println("Statement created. Executing getPrevious query...");
+			stmt.setString(1, token);
+			stmt.setString(2, "Active");
 			
 			ResultSet rs = stmt.executeQuery();
 			while(rs.next()) {
