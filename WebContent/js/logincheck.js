d2ba9af19e27073bd1dacdcdb611ef890c2a758d
AniruddhM <aniruddh@greylabs.org>
2015-12-16 15:53:39 +0530

    Added js file logincheck.js

---
+++ WebContent/js/logincheck.js
@@ -0,0 +1,25 @@
+function logInCheck() {
+	
+	if(userloggedin == "" || userloggedin == null){					//this will be required when database is empty
+				userloggedin = "anonymous";
+				localStorage.setItem("userloggedin", userloggedin);
+			}
+			
+			if(userloggedin == "anonymous"){	//if logged in as anonymous then don't display subheader
+				//$('#subheader').hide();		 
+				window.location.replace("mylogin.html");
+			}else{							//else show the subheader, and hide login and signup buttons	
+				$('#subheader').show();			
+				$("#loginoptionmenu").hide();
+				$("#signupoptionmenu").hide();
+				
+				//show "username"
+				var salutation = document.getElementById("salutation");
+				var span = document.createElement("span");
+				
+				span.innerHTML = userloggedin;
+				salutation.appendChild(span);
+				
+			}
+			//end of checking if user is logged in or not
+}
