8660432d3fce5e1058ac47cb8ed2dc1f962c04bb
AniruddhM <aniruddh@greylabs.org>
2015-12-16 14:57:44 +0530

    Did the following things.
    1) Added indentation to js code in dynamicdropdown.html file.
    2) Commented test function in dropdownlist.js file.
    3) Added </a> tag in myfrienddetails.html file.
    4) Removed extra js file link in myfriendslist.html file.

--- WebContent/js/dropdownlist.js
+++ WebContent/js/dropdownlist.js
@@ -1,13 +1,16 @@
 $(function(){
             $.get("dynamicdropdown.html", function(response) {
                   $('#dropA').html(response);
-				  applyDynamicBindings('#dropA');
+				  //applyDynamicBindings('#dropA');
             });
         });
 		
+		// Mentioned below is a test function for the above function.
+		/*
 		function applyDynamicBindings(container_selector){
              var $container=$(container_selector);
              $container.find("ul").click(function(){
                  //alert('triggered function bound to dynamic element');
              });
-        }
+        }*/
+		
