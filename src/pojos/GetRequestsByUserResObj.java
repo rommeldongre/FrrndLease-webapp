6364d1fae361c982482d234477cacb6cf6b30d98
AniruddhM <aniruddh@greylabs.org>
2015-12-22 11:56:06 +0000

    GetRequestsByUserResObj.java edited online with Bitbucket

--- src/pojos/GetRequestsByUserResObj.java
+++ src/pojos/GetRequestsByUserResObj.java
@@ -4,80 +4,162 @@ import javax.validation.constraints.NotNull;
 
 public class GetRequestsByUserResObj extends ResObj{
 	
-	@NotNull
+	//Return code for GetRequestsByUser
+	int ReturnCode = 0;
+	
+	//Error String
+	String ErrorString;
+	
+	
 	private String title;
 	
 	// description of item
 	private String desc;
 	
-	// Request Id of Of a Request
+	// number of items, default is 1
+	int quantity = 1;
+	
+	// category of item
 	// TBD: need to change type
 	@NotNull
-	int requestId;
+	String categoryId;
 	
-	// Id of a user
+	// user posting item
 	// TBD: change to user id type
 	@NotNull
-	String owneruserId;
+	String userId;
 	
-	// Date on which Item was posted
+	// Cookie
 	// TBD: change to user id type
 	@NotNull
-	String date;
+	int cookie;
+			
+	// term of lease: week/month/quarter/year/forever
+	//LeaseTermType leaseTerm;
+	String leaseTerm;
 	
-	// Token
-	// TBD: change to user id type
-	@NotNull
-	int token;
-
+	// reasonable replacement value in Indian Rs.
+	long leaseValue = 0;
+	/**
+	 * @return the title
+	 */
 	public String getTitle() {
 		return title;
 	}
-
+	/**
+	 * @param title the title to set
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
+	/**
+	 * @return the desc
+	 */
 	public String getDesc() {
 		return desc;
 	}
-
+	/**
+	 * @param desc the desc to set
+	 */
 	public void setDesc(String desc) {
 		this.desc = desc;
 	}
-
-	public int getRequestId() {
-		return requestId;
+	/**
+	 * @return the quantity
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	/**
+	 * @return the categoryId
+	 */
+	public String getCategoryId() {
+		return categoryId;
+	}
+	/**
+	 * @param string the categoryId to set
+	 */
+	public void setCategoryId(String string) {
+		this.categoryId = string;
+	}
+	/**
+	 * @return the userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	/**
+	 * @return the leaseValue
+	 */
+	public long getLeaseValue() {
+		return leaseValue;
+	}
+	/**
+	 * @param leaseValue the leaseValue to set
+	 */
+	public void setLeaseValue(long leaseValue) {
+		this.leaseValue = leaseValue;
 	}
 
-	public void setRequestId(int requestId) {
-		this.requestId = requestId;
+	/**
+	 * @return the userId
+	 */
+	/*
+	public String getCookie() {
+		return cookie;
+	}*/
+	/**
+	 * @param userId the userId to set
+	 */
+	/*
+	public void setCookie(String cookie) {
+		this.userId = cookie;
 	}
-
-	public String getOwneruserId() {
-		return owneruserId;
-	}
-
-	public void setOwneruserId(String owneruserId) {
-		this.owneruserId = owneruserId;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public int getToken() {
-		return token;
-	}
-
-	public void setToken(int token) {
-		this.token = token;
-	}
+	*/
 	
+	public int getReturnCode() {
+		return ReturnCode;
+	}
+	public int getCookie() {
+		return cookie;
+	}
+	public void setCookie(int cookie) {
+		this.cookie = cookie;
+	}
+	public void setReturnCode(int returnCode) {
+		ReturnCode = returnCode;
+	}
+	public String getErrorString() {
+		return ErrorString;
+	}
+	public void setErrorString(String errorString) {
+		ErrorString = errorString;
+	}
+	/*
+	public LeaseTermType getLeaseTerm() {
+		return leaseTerm;
+	}
+	public void setLeaseTerm(LeaseTermType leaseTerm) {
+		this.leaseTerm = leaseTerm;
+	}*/
+	public String getLeaseTerm() {
+		return leaseTerm;
+	}
+	public void setLeaseTerm(String leaseTerm) {
+		this.leaseTerm = leaseTerm;
+	}
 	
 	
 }
