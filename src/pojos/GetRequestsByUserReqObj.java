c4dade2e3727857bca25b39f9aeec3f8470e48d4
AniruddhM <aniruddh@greylabs.org>
2015-12-22 11:26:27 +0000

    GetRequestsByUserReqObj.java edited online with Bitbucket

--- src/pojos/GetRequestsByUserReqObj.java
+++ src/pojos/GetRequestsByUserReqObj.java
@@ -4,34 +4,39 @@ import javax.validation.constraints.NotNull;
 
 public class GetRequestsByUserReqObj extends ReqObj {
 	// title of item
+		// user posting item
+		@NotNull
+		String userId;
+		
+		// Cookie
+		// TBD: change to user id type
+		@NotNull
+		int cookie;
+				
+		public void setCookie(int cookie) {
+			this.cookie = cookie;
+		}
+		
+		/**
+		 * @return the userId
+		 */
+		public String getUserId() {
+			return userId;
+		}
+		/**
+		 * @param userId the userId to set
+		 */
+		public void setUserId(String userId) {
+			this.userId = userId;
+		}
+		/**
+		 * @return the userId
+		 */
+		public int getCookie() {
+			return cookie;
+		}
+		/**
+		 * @param userId the userId to set
+		 */
 	
-	// Id of a user
-		// TBD: change to user id type
-		@NotNull
-		String requestoruserId;
-		
-		// Token
-		// TBD: change to user id type
-		@NotNull
-		int token;
-
-		public String getRequestoruserId() {
-			return requestoruserId;
-		}
-
-		public void setRequestoruserId(String requestoruserId) {
-			this.requestoruserId = requestoruserId;
-		}
-
-		public int getToken() {
-			return token;
-		}
-
-		public void setToken(int token) {
-			this.token = token;
-		}
-		
-		
-		
-		
-}
+	}
