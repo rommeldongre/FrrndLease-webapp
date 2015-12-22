f104b086275f51a463a1d033604650b21f019084
AniruddhM <aniruddh@greylabs.org>
2015-12-22 11:24:29 +0000

    GetRequestsByUser.java edited online with Bitbucket

--- src/services/GetRequestsByUser.java
+++ src/services/GetRequestsByUser.java
@@ -1,4 +1,4 @@
-package services;
+package service;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,37 +14,21 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.util.JSONPObject;
 import org.json.JSONObject;
 
-import connect.Connect;
-import errorCat.ErrorCat;
-import adminOps.AdminOpsHandler;
-import adminOps.Response;
-import app.GetRequestsByUserHandler;
-import app.NotImplementedException;
 import pojos.GetRequestsByUserReqObj;
 import pojos.GetRequestsByUserResObj;
+import app.GetRequestsByUserHandler;
+import app.NotImplementedException;
 
 
 /**
  * Servlet implementation class GetRequestsByUser
  */
-/*@WebServlet(description = "List Items from the Store By User.", urlPatterns = { "/GetRequestsByUser" })*/
-@WebServlet("/GetRequestsByUser")
+@WebServlet(description = "List Item Requests for a given UserId", urlPatterns = { "/GetRequestsByUser" })
 public class GetRequestsByUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private GetRequestsByUserHandler grbuh = new GetRequestsByUserHandler();
-	private Response res = new Response();
-	private ErrorCat e = new ErrorCat();
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	
-	protected void doGet(HttpServletRequest httprequest, HttpServletResponse httpresponse) throws ServletException, IOException {
-		httpresponse.setContentType("application/json");
-		System.out.println("Inside GET Method");
-		
-		doPost(httprequest,httpresponse);
-	}
-	
 	protected void doPost(HttpServletRequest httprequest, HttpServletResponse httpresponse) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 				
@@ -54,38 +38,18 @@ public class GetRequestsByUser extends HttpServlet {
 			httpresponse.setContentType("application/json");
 			
 			// application logic comes here --------		
-			GetRequestsByUserResObj getResponse = null;
+			GetRequestsByUserResObj Response = null;
 		
 			try {
 				//App handler to process request and create Service response pojo
-				getResponse = (GetRequestsByUserResObj) GetRequestsByUserHandler.getInstance().process(request);
+				Response = (GetRequestsByUserResObj) GetRequestsByUserHandler.getInstance().process(request);
 				
 				//Service response pojo to JSON
-				/*
 				PrintWriter out = httpresponse.getWriter();
-				JSONObject ResponseObj=new JSONObject();*/
-				
-				/* sample code. replace with actual mapping from response pojo to JSON 
-				ResponseObj.put("title", getResponse.getTitle());
-				ResponseObj.put("desc", getResponse.getDesc());
-				ResponseObj.put("ReturnCode", getResponse.getReturnCode());
-				ResponseObj.put("ErrorString", getResponse.getErrorString());
-				ResponseObj.put("userId", getResponse.getUserId());
-				ResponseObj.put("categoryId", getResponse.getCategoryId());
-				ResponseObj.put("quantity", getResponse.getQuantity());
-				ResponseObj.put("leaseTerm", getResponse.getLeaseTerm());
-				ResponseObj.put("leaseValue", getResponse.getLeaseValue());
-				ResponseObj.put("cookie",getResponse.getCookie());
-				*/
-				
-				
-				/*httpresponse.setContentType("text/json");				
-				PrintWriter printout = httpresponse.getWriter();
-				printout.print(response.toString());*/	
-				
-				ObjectMapper mapper1 = new ObjectMapper();
-				mapper1.writeValue(httpresponse.getOutputStream(), getResponse);
-				httpresponse.setContentType("application/json; charset=UTF-8");
+				httpresponse.setContentType("text/json");				
+				httpresponse.setContentType("application/json; charset=UTF-8");	
+				mapper.writeValue(out, Response);
+
 					
 			} catch (NotImplementedException e) {
 				e.printStackTrace();
