5b75147f3f188e005409014a515fe91562b35503
AniruddhM <aniruddh@greylabs.org>
2016-01-15 20:42:22 +0530

    Added multiple switch cases in FlsSendMail.java file for sending different kinds of notification.

--- src/util/FlsSendMail.java
+++ src/util/FlsSendMail.java
@@ -10,7 +10,23 @@ public class FlsSendMail {
 	
 	private static String user_id;
 	
-	public enum Fls_Enum { FLS_MAIL_REGISTER, FLS_MAIL_POST_ITEM, FLS_MAIL_DELETE_ITEM, FLS_MAIL_GRANT_REQUEST, FLS_MAIL_REJECT_REQUEST, FLS_MAIL_ADD_FRIEND, FLS_MAIL_DELETE_FRIEND, FLS_MAIL_GRANT_lEASE, FLS_MAIL_REJECT_lEASE   }
+	public enum Fls_Enum { FLS_MAIL_REGISTER, //done
+						   FLS_MAIL_POST_ITEM, //done
+						   FLS_MAIL_DELETE_ITEM, //done
+						   FLS_MAIL_MAKE_REQUEST_FROM, //done
+						   FLS_MAIL_MAKE_REQUEST_TO, // not done as pojo needs to be filled
+						   FLS_MAIL_GRANT_REQUEST_FROM, //same as grant lease from
+						   FLS_MAIL_GRANT_REQUEST_TO, //same as grant lease to
+						   FLS_MAIL_REJECT_REQUEST_FROM, // working partially. Set flag for lease part
+						   FLS_MAIL_REJECT_REQUEST_TO, // not done as pojo needs to be filled
+						   FLS_MAIL_ADD_FRIEND_FROM, //done
+						   FLS_MAIL_ADD_FRIEND_TO, //done
+						   FLS_MAIL_DELETE_FRIEND_FROM, //done
+						   FLS_MAIL_DELETE_FRIEND_TO, //done
+						   FLS_MAIL_GRANT_LEASE_FROM,//done
+						   FLS_MAIL_GRANT_LEASE_TO,//done
+						   FLS_MAIL_REJECT_LEASE_FROM, // not done pojo needs to be filled
+						   FLS_MAIL_REJECT_LEASE_TO } //done
 	public static void send(String userId, Fls_Enum fls_enum, Object obj) throws Exception {
 		//Fls_Enum = fls_enum;
 		user_id = userId;
@@ -30,12 +46,102 @@ public class FlsSendMail {
 			email.setMsg(uom.getFullName()+" You Have signed up ... :-)");
 			break;
 			
+		case FLS_MAIL_DELETE_ITEM:
+			ItemsModel idom = (ItemsModel) obj;
+			email.setSubject("Item ["+idom.getTitle()+"] deleted from friendlease");
+			email.setMsg("You have deleted a item, the details of which are:- \n \n"+
+			              " Title : "+idom.getTitle()+"\n"+
+			              " Category : "+idom.getCategory()+"\n"+
+			              " Description : "+idom.getDescription()+"\n"+
+			              " Lease Value : "+idom.getLeaseValue()+"\n"+
+					      " Lease Term : "+idom.getLeaseTerm()+"\n"+
+					      " Status : "+idom.getStatus());      
+			break;
+			
 		case FLS_MAIL_POST_ITEM:
 			ItemsModel iom = (ItemsModel) obj;
-			email.setSubject("Item "+iom.getTitle()+" Added to friendlease");
-			email.setMsg("Item "+iom.getTitle()+" belonging to category "+iom.getCategory()+" has been added");
+			email.setSubject("Item ["+iom.getTitle()+"] added to friendlease");
+			email.setMsg("You have added an item, the details of which are:- \n \n"+
+			              " Title : "+iom.getTitle()+"\n"+
+			              " Category : "+iom.getCategory()+"\n"+
+			              " Description : "+iom.getDescription()+"\n"+
+			              " Lease Value : "+iom.getLeaseValue()+"\n"+
+					      " Lease Term : "+iom.getLeaseTerm()+"\n"+
+					      " Status : "+iom.getStatus());      
 			break;
-
+			
+		case FLS_MAIL_ADD_FRIEND_FROM:
+			FriendsModel affm = (FriendsModel) obj;
+			email.setSubject("["+affm.getFullName()+"] added to Friendlist");
+			email.setMsg("You have sucessfully added ["+affm.getFullName()+"] to your Friendlist- \n \n");      
+			break;
+		
+		case FLS_MAIL_ADD_FRIEND_TO:
+			FriendsModel atfm = (FriendsModel) obj;
+			email.setSubject("["+atfm.getFriendId()+"] added you");
+			email.setMsg("You have been connected to ["+atfm.getFriendId()+"] on Friend Lease- \n \n");      
+			break;
+			
+		case FLS_MAIL_DELETE_FRIEND_FROM:
+			FriendsModel dffm = (FriendsModel) obj;
+			email.setSubject(dffm.getUserId()+" removed from Friendlist");
+			email.setMsg("You have sucessfully removed "+dffm.getUserId()+" from your Friendlist- \n \n");      
+			break;
+			
+		case FLS_MAIL_DELETE_FRIEND_TO:
+			FriendsModel dtfm = (FriendsModel) obj;
+			email.setSubject("["+dtfm.getFriendId()+"] removed You");
+			email.setMsg("You have been removed from Friendlist of user "+dtfm.getFriendId()+"- \n \n");      
+			break;
+			
+		case FLS_MAIL_REJECT_REQUEST_FROM:
+			RequestsModel dfrm = (RequestsModel) obj;
+			email.setSubject("Request removed");
+			email.setMsg("Item request has been removed sucessfully. \n \n");      
+			break;
+			
+		case FLS_MAIL_REJECT_REQUEST_TO:
+			RequestsModel dtrm = (RequestsModel) obj;
+			email.setSubject("Request removed");
+			email.setMsg("An item request has been removed. \n \n");      
+			break;
+			
+		case FLS_MAIL_GRANT_LEASE_FROM:
+			LeasesModel gflm = (LeasesModel) obj;
+			email.setSubject("Lease granted to user ["+gflm.getReqUserId()+"]");
+			email.setMsg("You have sucessfully leased an item to ["+gflm.getReqUserId()+"] on Friend Lease - \n \n");      
+			break;
+			
+		case FLS_MAIL_GRANT_LEASE_TO:
+			LeasesModel gtlm = (LeasesModel) obj;
+			email.setSubject("Lease granted to you by ["+gtlm.getUserId()+"]");
+			email.setMsg("An item has been leased by ["+gtlm.getUserId()+"] to you on Friend Lease - \n \n");      
+			break;
+			
+		case FLS_MAIL_REJECT_LEASE_FROM:
+			LeasesModel rflm = (LeasesModel) obj;
+			email.setSubject("Lease granted to user ["+rflm.getReqUserId()+"] Rejected");
+			email.setMsg("You have sucessfully leased an item to ["+rflm.getReqUserId()+"] on Friend Lease - \n \n");      
+			break;
+			
+		case FLS_MAIL_REJECT_LEASE_TO:
+			LeasesModel rtlm = (LeasesModel) obj;
+			email.setSubject("Lease Closed by the other party");
+			email.setMsg("Lease has been closed by the other party for an item on Friend Lease - \n \n");      
+			break;
+		 
+		case FLS_MAIL_MAKE_REQUEST_FROM:
+			RequestsModel rfrm = (RequestsModel) obj;
+			email.setSubject("Item Requested");
+			email.setMsg("You have sucessfully Requested an item on Friend Lease - \n \n");      
+			break;
+			
+		case FLS_MAIL_MAKE_REQUEST_TO:
+			ItemsModel irtm = (ItemsModel) obj;
+			email.setSubject("Item Requested");
+			email.setMsg("The Item having id"+irtm.getId()+" has been requested on Friend Lease - \n \n");      
+			break;
+			
 		default:
 			email.setSubject("Default Subject");
 			email.setMsg("Default Message ... :-)");
