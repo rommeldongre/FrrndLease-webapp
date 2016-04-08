c2201c0db1cb0566f25e0d8bb142c9376eeef250
Ankit Karnany <ankit@greylabs.org>
2016-04-08 14:37:40 +0530

    my profile both backend and frontend implemented

---
+++ src/pojos/EditProfileReqObj.java
@@ -0,0 +1,39 @@
+package pojos;
+
+public class EditProfileReqObj extends ReqObj {
+
+	String userId, fullName, mobile, location;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
