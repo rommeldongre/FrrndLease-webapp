ced9a0b8b4ba4e319662a021eefd63f02e5841d7
AniruddhM <aniruddh@greylabs.org>
2016-04-27 18:19:54 +0530

    Added query to create config table.

--- fls_update.sql
+++ fls_update.sql
@@ -6,3 +6,10 @@ ALTER TABLE items MODIFY item_desc VARCHAR(255) null;
 
 /* 1.user_activation for saving the activation id 2.user_status column for sign up data and status code*/
 ALTER TABLE `users` ADD `user_activation` VARCHAR(255) NOT NULL AFTER `user_auth`, ADD `user_status` VARCHAR(255) NOT NULL AFTER `user_activation`;
+
+/* Query to create config table*/
+CREATE TABLE `config` (
+  `option` varchar(255) NOT NULL,
+  `value` varchar(1024) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY
+);
