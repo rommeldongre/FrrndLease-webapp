0c2860c1df4c5fb677b3a17271e851bab6795466
AniruddhM <aniruddh@greylabs.org>
2016-05-23 15:25:41 +0530

    Added working app Handler code with updated admin page.

---
+++ src/pojos/GetRequestsResObj.java
@@ -0,0 +1,56 @@
+package pojos;
+
+public class GetRequestsResObj extends ResObj{
+	
+	int Code, RequestItemId,requestId;
+	String Id,Message, RequestUserId, RequestDate, Title;
+	public int getCode() {
+		return Code;
+	}
+	public void setCode(int code) {
+		Code = code;
+	}
+	public int getRequestId() {
+		return requestId;
+	}
+	public void setRequestId(int requestId) {
+		this.requestId = requestId;
+	}
+	public String getId() {
+		return Id;
+	}
+	public void setId(String id) {
+		Id = id;
+	}
+	public String getMessage() {
+		return Message;
+	}
+	public void setMessage(String message) {
+		Message = message;
+	}
+	
+	public int getRequestItemId() {
+		return RequestItemId;
+	}
+	public void setRequestItemId(int requestItemId) {
+		RequestItemId = requestItemId;
+	}
+	public String getRequestUserId() {
+		return RequestUserId;
+	}
+	public void setRequestUserId(String requestUserId) {
+		RequestUserId = requestUserId;
+	}
+	public String getRequestDate() {
+		return RequestDate;
+	}
+	public void setRequestDate(String requestDate) {
+		RequestDate = requestDate;
+	}
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+}
