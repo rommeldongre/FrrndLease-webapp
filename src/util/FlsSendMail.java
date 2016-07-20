98c99a885edbb8753c79f124b969dff56c8374f1
AniruddhM <aniruddh@greylabs.org>
2016-07-20 18:14:08 +0530

    Added new enums in FlsSendMail.java

--- src/util/FlsSendMail.java
+++ src/util/FlsSendMail.java
@@ -32,7 +32,10 @@ public class FlsSendMail {
 		FLS_MAIL_GRANT_LEASE_FROM, // done
 		FLS_MAIL_GRANT_LEASE_TO, // done
 		FLS_MAIL_REJECT_LEASE_FROM, // not done pojo needs to be filled //done
-		FLS_MAIL_REJECT_LEASE_TO
+		FLS_MAIL_REJECT_LEASE_TO,
+		
+		FLS_MAIL_GRACE_PERIOD_OWNER,
+		FLS_MAIL_GRACE_PERIOD_REQUESTOR
 	} // done
 
 	public static void send(String userId, Fls_Enum fls_enum, Object obj, String... apiflag) throws Exception {
