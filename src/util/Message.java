2a17038d82bcfe36277a057e0a3917f7c5b4f483
Ankit Karnany <ankit@greylabs.org>
2016-11-16 18:36:29 +0530

    request message ui change in myincomingrequests

--- src/util/Message.java
+++ src/util/Message.java
@@ -16,16 +16,9 @@ public class Message extends Connect {
 	
 	private int itemId;
 	
-	private String uid = "", title = "", fromName = "", toName = "";
-	
 	public enum Subject{
 		FRIEND,
 		ITEM
-	}
-	
-	public enum User{
-		FROM,
-		TO
 	}
 	
 	public Message(){}
@@ -37,14 +30,46 @@ public class Message extends Connect {
 	public int sendMessage(String from, String to, Subject subject, String message){
 		LOGGER.info("Inside sendMessage Method");
 		
-		getUserName(from, User.FROM);
-		getUserName(to, User.TO);
+		String fromName = getUserName(from);
+		String toName = getUserName(to);
 		
 		try{
 			Event event = new Event();
 			switch(subject){
 				case ITEM:
-					getItemDetails();
+					String uid = "", title = "";
+					
+					Connection hcp = getConnectionFromPool();
+					PreparedStatement ps1 = null;
+					ResultSet rs1 = null;
+					
+					try{
+						String sqlGetUserName = "SELECT item_name, item_uid FROM items WHERE item_id=?";
+						ps1 = hcp.prepareStatement(sqlGetUserName);
+						ps1.setInt(1, itemId);
+						
+						rs1 = ps1.executeQuery();
+						
+						if(rs1.next()){
+							title = rs1.getString("item_name");
+							uid = rs1.getString("item_uid");
+						}else{
+							LOGGER.warning("Not Able to find itemId : " + itemId);
+						}
+						
+					}catch(Exception e){
+						LOGGER.warning("Error occured while getting item title and uid");
+						e.printStackTrace();
+					}finally{
+						try{
+							if(rs1 != null) rs1.close();
+							if(ps1 != null) ps1.close();
+							if(hcp != null) hcp.close();
+						}catch(Exception e){
+							e.printStackTrace();
+						}
+					}
+					
 					LOGGER.info("Sending message to item's owner : " + to);
 					if(itemId != 0){
 						event.createEvent(to, from, Event_Type.FLS_EVENT_NOTIFICATION, Notification_Type.FLS_MAIL_MESSAGE_ITEM_FROM, itemId, "From: <i>You</i>. To: <i>" + toName + "</i>. Regarding: <a href=\"" + URL + "/ItemDetails?uid=" + uid + "\">" + title + "</a>. <br> <i>" + message + "'</i>");
@@ -68,7 +93,7 @@ public class Message extends Connect {
 		return 0;
 	}
 	
-	private void getUserName(String userId, User user){
+	private String getUserName(String userId){
 		
 		LOGGER.info("Inside getUserName Method");
 		
@@ -84,11 +109,7 @@ public class Message extends Connect {
 			rs1 = ps1.executeQuery();
 			
 			if(rs1.next()){
-				String name = rs1.getString("user_full_name");
-				if(user.name().equals(User.FROM.name()))
-					fromName = name;
-				else if(user.name().equals(User.TO.name()))
-					toName = name;
+				return rs1.getString("user_full_name");
 			}else{
 				LOGGER.warning("Not Able to find userId : " + userId);
 			}
@@ -105,44 +126,7 @@ public class Message extends Connect {
 				e.printStackTrace();
 			}
 		}
-		
-	}
-	
-	private void getItemDetails(){
-		
-		LOGGER.info("Inside getItemDetails Method");
-		
-		Connection hcp = getConnectionFromPool();
-		PreparedStatement ps1 = null;
-		ResultSet rs1 = null;
-		
-		try{
-			String sqlGetUserName = "SELECT item_name, item_uid FROM items WHERE item_id=?";
-			ps1 = hcp.prepareStatement(sqlGetUserName);
-			ps1.setInt(1, itemId);
-			
-			rs1 = ps1.executeQuery();
-			
-			if(rs1.next()){
-				title = rs1.getString("item_name");
-				uid = rs1.getString("item_uid");
-			}else{
-				LOGGER.warning("Not Able to find itemId : " + itemId);
-			}
-			
-		}catch(Exception e){
-			LOGGER.warning("Error occured while getting item title and uid");
-			e.printStackTrace();
-		}finally{
-			try{
-				if(rs1 != null) rs1.close();
-				if(ps1 != null) ps1.close();
-				if(hcp != null) hcp.close();
-			}catch(Exception e){
-				e.printStackTrace();
-			}
-		}
-		
+		return null;
 	}
 	
 }
