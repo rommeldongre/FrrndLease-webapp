e833d0d8cb8753ace09e72b92d4b09b1ce0eb965
Ankit Karnany <ankit@greylabs.org>
2016-11-23 15:28:06 +0530

    new design of myoutgoingrequests implemented

--- src/app/GetRequestsByUserHandler.java
+++ src/app/GetRequestsByUserHandler.java
@@ -3,7 +3,6 @@ package app;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import connect.Connect;
 import pojos.GetRequestsByUserReqObj;
@@ -11,15 +10,10 @@ import pojos.GetRequestsByUserResObj;
 import pojos.ReqObj;
 import pojos.ResObj;
 import util.FlsLogger;
-import adminOps.Response;
 
 public class GetRequestsByUserHandler extends Connect implements AppHandler {
 
 	private FlsLogger LOGGER = new FlsLogger(GetRequestsByUserHandler.class.getName());
-
-	private String user_name, check = null, Id = null, token, message;
-	private int Code;
-	private Response res = new Response();
 
 	private static GetRequestsByUserHandler instance = null;
 
@@ -31,81 +25,85 @@ public class GetRequestsByUserHandler extends Connect implements AppHandler {
 
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public ResObj process(ReqObj req) throws Exception {
-		// TODO Auto-generated method stub
+		
+		LOGGER.info("Inside Process Method of GetRequestsByUserHandler");
+		
 		GetRequestsByUserReqObj rq = (GetRequestsByUserReqObj) req;
 		GetRequestsByUserResObj rs = new GetRequestsByUserResObj();
+		
 		Connection hcp = getConnectionFromPool();
-
-		LOGGER.info("Inside process method " + rq.getUserId() + ", " + rq.getCookie());
-		// TODO: Core of the processing takes place here
-		check = null;
-		LOGGER.info("Inside GetOutgoingrequests method");
-
+		PreparedStatement ps1 = null;
+		ResultSet rs1 = null;
+		
 		try {
-			String sql = "SELECT tb1.request_lastmodified, tb1.request_item_id, tb1.request_id, tb1.request_status, tb2.item_name, tb2.item_desc, tb2.item_user_id, tb2.item_category, tb2.item_lease_value, tb2.item_lease_term, tb2.item_primary_image_link, tb2.item_uid, tb3.user_full_name, tb3.user_mobile, tb3.user_address, tb3.user_locality, tb3.user_sublocality FROM requests tb1 INNER JOIN items tb2 on tb1.request_item_id = tb2.item_id INNER JOIN users tb3 on tb2.item_user_id = tb3.user_id WHERE tb1.request_requser_id=? AND tb1.request_id>? HAVING tb1.request_status=? ORDER by tb1.request_id ASC LIMIT 1";
-			LOGGER.info("Creating a statement .....");
-			PreparedStatement stmt = hcp.prepareStatement(sql);
+			String userId = rq.getUserId();
+			int offset = rq.getCookie();
+			
+			String sql = "SELECT tb1.request_id, tb1.request_lastmodified, tb1.request_message, tb2.*, tb3.*, (CASE WHEN tb2.item_user_id=tb4.friend_id AND tb4.friend_user_id=tb1.request_requser_id THEN true ELSE false END) AS isFriend, ( 6371 * acos( cos( radians(tb2.item_lat) ) * cos( radians( tb5.user_lat ) ) * cos( radians( tb5.user_lng ) - radians(tb2.item_lng) ) + sin( radians(tb2.item_lat) ) * sin( radians( tb5.user_lat ) ) ) ) AS distance FROM requests tb1 INNER JOIN items tb2 on tb1.request_item_id = tb2.item_id INNER JOIN users tb3 on tb2.item_user_id = tb3.user_id LEFT JOIN (SELECT * FROM friends WHERE friend_user_id=?) tb4 ON tb2.item_user_id = tb4.friend_id INNER JOIN users tb5 ON tb1.request_requser_id = tb5.user_id WHERE tb1.request_status=? AND tb1.request_requser_id=? ORDER BY tb1.request_lastmodified LIMIT ?, 1";
+			ps1 = hcp.prepareStatement(sql);
 
-			LOGGER.info("Statement created. Executing GetOutgoingrequests query...");
-			stmt.setString(1, rq.getUserId());
-			stmt.setInt(2, rq.getCookie());
-			stmt.setString(3, "Active");
+			ps1.setString(1, userId);
+			ps1.setString(2, "Active");
+			ps1.setString(3, userId);
+			ps1.setInt(4, offset);
 
-			ResultSet dbResponse = stmt.executeQuery();
-
-			if (dbResponse.next()) {
-				check = dbResponse.getString("request_item_id");
-
-				if (check != null) {
-					// Populate the response
-					rs.setTitle(dbResponse.getString("item_name"));
-					rs.setDesc(dbResponse.getString("item_desc"));
-					rs.setOwner_Id(dbResponse.getString("item_user_id"));
-					rs.setRequest_status(dbResponse.getString("request_status"));
-					rs.setRequest_id(dbResponse.getInt("request_id"));
-					rs.setRequest_item_id(dbResponse.getInt("request_item_id"));
-					rs.setRequest_date(dbResponse.getString("request_lastmodified"));
-					rs.setOwner_name(dbResponse.getString("user_full_name"));
-					rs.setCategory(dbResponse.getString("item_category"));
-					rs.setLeaseValue(dbResponse.getString("item_lease_value"));
-					rs.setLeaseTerm(dbResponse.getString("item_lease_term"));
-					rs.setPrimaryImageLink(dbResponse.getString("item_primary_image_link"));
-					rs.setUid(dbResponse.getString("item_uid"));
-					rs.setOwner_mobile(dbResponse.getString("user_mobile"));
-					rs.setOwner_address(dbResponse.getString("user_address"));
-					rs.setOwner_locality(dbResponse.getString("user_locality"));
-					rs.setOwner_sublocality(dbResponse.getString("user_sublocality"));
-
-					message = rs.getTitle() + ", " + rs.getDesc() + ", " + rs.getOwner_Id() + ", "
-							+ rs.getRequest_status() + ", " + rs.getRequest_item_id() + ", " + rs.getRequest_date();
-					LOGGER.info("Printing out Resultset: " + message);
-					Code = FLS_SUCCESS;
-					Id = check;
-				} else {
-					Id = "0";
-					message = FLS_END_OF_DB_M;
-					Code = FLS_END_OF_DB;
-					rs.setErrorString("End of table reached");
-				}
+			rs1 = ps1.executeQuery();
+			
+			if(rs1.next()){
+				// Item Details
+				rs.setItemId(rs1.getInt("item_id"));
+				rs.setTitle(rs1.getString("item_name"));
+				rs.setDescription(rs1.getString("item_desc"));
+				rs.setCategory(rs1.getString("item_category"));
+				rs.setLeaseTerm(rs1.getString("item_lease_term"));
+				rs.setInsurance(rs1.getInt("item_lease_value"));
+				rs.setPrimaryImageLink(rs1.getString("item_primary_image_link"));
+				rs.setUid(rs1.getString("item_uid"));
+				
+				// Request Details
+				rs.setRequestId(rs1.getInt("request_id"));
+				rs.setRequestDate(rs1.getString("request_lastmodified"));
+				rs.setMessage(rs1.getString("request_message"));
+				
+				// Owner Details
+				rs.setOwnerId(rs1.getString("user_id"));
+				rs.setOwnerName(rs1.getString("user_full_name"));
+				rs.setOwnerProfilePic(rs1.getString("user_profile_picture"));
+				rs.setOwnerLocality(rs1.getString("user_locality"));
+				rs.setOwnerSublocality(rs1.getString("user_sublocality"));
+				rs.setDistance(rs1.getFloat("distance"));
+				rs.setFriend(rs1.getBoolean("isFriend"));
+				
+				rs.setOffset(offset + 1);
+				rs.setCode(FLS_SUCCESS);
+				rs.setMessage(FLS_SUCCESS_M);
+			}else{
+				rs.setCode(FLS_END_OF_DB);
+				rs.setMessage(FLS_END_OF_DB_M);
 			}
-
-			stmt.close();
-			// res.setData(Code,Id,message);
-		} catch (SQLException e) {
-			res.setData(FLS_SQL_EXCEPTION, "0", FLS_SQL_EXCEPTION_M);
+			
+		} catch (Exception e) {
+			rs.setCode(FLS_SQL_EXCEPTION);
+			rs.setMessage(FLS_SQL_EXCEPTION_M);
 			LOGGER.warning("Error Check Stacktrace");
 			e.printStackTrace();
 		} finally {
-			hcp.close();
+			try{
+				if(rs1 != null) rs1.close();
+				if(ps1 != null) ps1.close();
+				if(hcp != null) hcp.close();
+			}catch(Exception e){
+				e.printStackTrace();
+				rs.setCode(FLS_INVALID_OPERATION);
+				rs.setMessage(FLS_END_OF_DB_M);
+			}
 		}
 
-		LOGGER.info("Finished process method ");
+		LOGGER.info("Finished process method");
 		// return the response
 		return rs;
 
@@ -113,6 +111,5 @@ public class GetRequestsByUserHandler extends Connect implements AppHandler {
 
 	@Override
 	public void cleanup() {
-		// TODO Auto-generated method stub
 	}
 }
