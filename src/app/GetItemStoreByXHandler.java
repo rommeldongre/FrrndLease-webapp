ca4ae905ac3df948385b0560037a91c839995181
Ankit Karnany <ankit@greylabs.org>
2016-12-06 16:25:08 +0530

    removed hardcoded admin user ids

--- src/app/GetItemStoreByXHandler.java
+++ src/app/GetItemStoreByXHandler.java
@@ -87,17 +87,13 @@ public class GetItemStoreByXHandler extends Connect implements AppHandler {
 			if(category != null)
 				sql = sql + " AND tb1.item_category='"+category+"'";
 			
-			if(userId != null && !(userId.equals("admin@frrndlease.com")) && !(userId.equals("ops@frrndlease.com")))
+			if(userId != null)
 				sql = sql + " AND tb1.item_user_id='"+userId+"'";
 			
 			if(searchString != "" || searchString != null)
 				sql = sql + " AND (tb1.item_name LIKE '%"+searchString+"%' OR tb1.item_desc LIKE '%"+searchString+"%')";
 			
-			if(userId != null && (userId.equals("admin@frrndlease.com") || userId.equals("ops@frrndlease.com"))){
-				sql = sql + " ORDER BY tb1.item_id DESC LIMIT "+offset+", "+limit;
-			}else{
-				sql = sql + " ORDER BY friendst DESC, distance, tb1.item_id DESC LIMIT "+offset+", "+limit;
-			}
+			sql = sql + " ORDER BY friendst DESC, distance, tb1.item_id DESC LIMIT "+offset+", "+limit;
 			
 			sql_stmt = hcp.prepareStatement(sql);
 
