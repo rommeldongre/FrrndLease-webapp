86eea4f34d9a96196c0236810220f884f90a8266
Ankit Karnany <ankit@greylabs.org>
2016-12-20 20:13:14 +0530

    add promo code implemented with new columns

--- src/tableOps/PromoCode.java
+++ src/tableOps/PromoCode.java
@@ -10,14 +10,18 @@ import org.json.JSONObject;
 import adminOps.Response;
 import connect.Connect;
 import pojos.PromoCodeModel;
+import pojos.PromoCodeModel.Code_Type;
 import util.FlsLogger;
+import util.OAuth;
 
 public class PromoCode extends Connect {
 
 	private FlsLogger LOGGER = new FlsLogger(PromoCode.class.getName());
 
-	private String operation, promoCode, expiry, userId;
-	private int credit, token;
+	private String operation, promoCode, expiry, userId, accessToken;
+	private int credit, count, perPersonCount;
+	
+	private Code_Type codeType;
 
 	private PromoCodeModel pcm;
 	private Response res = new Response();
@@ -42,18 +46,30 @@ public class PromoCode extends Connect {
 	}
 	
 	private void addPromoCode(){
-		promoCode = pcm.getPromoCode();
-		credit = pcm.getCredit();
-		expiry = pcm.getExpiry();
 		
 		LOGGER.info("Inside addPromoCode Method");
 		
+		userId = pcm.getUserId();
+		accessToken = pcm.getAccessToken();
+		
+		OAuth oauth = new OAuth();
+		String oauthcheck = oauth.CheckOAuth(accessToken);
+		if(!oauthcheck.equals(userId)){
+			res.setData(FLS_ACCESS_TOKEN_FAILED, "0", FLS_ACCESS_TOKEN_FAILED_M);
+			return;
+		}
+		
+		promoCode = pcm.getPromoCode();
+		expiry = pcm.getExpiry();
+		codeType = pcm.getCodeType();
+		credit = pcm.getCredit();
+		count = pcm.getCount();
+		perPersonCount = pcm.getPerPersonCount();
+		
 		Connection hcp = getConnectionFromPool();
 		
-		PreparedStatement ps1 = null;
+		PreparedStatement ps1 = null, ps2 = null;
 		ResultSet rs1 = null;
-		
-		PreparedStatement ps2 = null;
 		
 		try{
 			
@@ -66,11 +82,20 @@ public class PromoCode extends Connect {
 			if(rs1.next()){
 				res.setData(FLS_DUPLICATE_ENTRY, "0", FLS_DUPLICATE_ENTRY_M);
 			}else{
-				String sqlAddPromoCode = "INSERT INTO `promo_credits`(`code`, `credit`, `expiry`) VALUES (?,?,?)";
+				String sqlAddPromoCode = "INSERT INTO `promo_credits` (`code`, `credit`, `expiry`, `count`, `per_person_count`, `code_type`) VALUES (?, ?, ?, ?, ?, ?)";
 				ps2 = hcp.prepareStatement(sqlAddPromoCode);
 				ps2.setString(1, promoCode);
 				ps2.setInt(2, credit);
 				ps2.setString(3, expiry);
+				if(count == -1)
+					ps2.setString(4, null);
+				else
+					ps2.setInt(4, count);
+				if(perPersonCount == -1)
+					ps2.setString(5, null);
+				else
+					ps2.setInt(5, perPersonCount);
+				ps2.setString(6, codeType.name());
 				
 				ps2.executeUpdate();
 				
@@ -86,8 +111,7 @@ public class PromoCode extends Connect {
 				if(rs1 != null)rs1.close();
 				if(ps1 != null)ps1.close();
 				if(hcp != null)hcp.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
