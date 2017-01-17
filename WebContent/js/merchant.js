4be012823e4c63d4b8073df8fc3ee460e600b6fe
Ankit Karnany <ankit@greylabs.org>
2017-01-17 12:17:41 +0530

    steps UI added for onboarding wizard

--- WebContent/js/merchant.js
+++ WebContent/js/merchant.js
@@ -1,17 +1,6 @@
-var merchantApp = angular.module('merchantApp', ['headerApp', 'footerApp', 'ngAutocomplete', 'multiStepForm']);
+var merchantApp = angular.module('merchantApp', ['headerApp', 'footerApp', 'ngAutocomplete']);
 
 merchantApp.controller('merchantCtrl', ['$scope', 'modalService', 'userFactory', 'matchFbIdService', function($scope, modalService, userFactory, matchFbIdService){
-
-    $scope.steps = [
-        {
-            title: 'Sign Up',
-            templateUrl: 'merchant_signup.html'
-        },
-        {
-            title: 'Pay Membership',
-            templateUrl: 'merchant_payment.html'
-        }
-    ];
 
     // sign up scope object
     $scope.user = {
@@ -19,7 +8,9 @@ merchantApp.controller('merchantCtrl', ['$scope', 'modalService', 'userFactory',
         userId: '',
         referral: '',
         location: '',
-        email: ''
+        email: '',
+        signedUp: false,
+        verified: false
     };
 
     // sign up request object
@@ -41,6 +32,11 @@ merchantApp.controller('merchantCtrl', ['$scope', 'modalService', 'userFactory',
         lng: "0.0",
         friendId: ""
     }
+    
+    $scope.$watch('onboarding', function(){
+        if($scope.onboarding == true)
+            $("html, body").animate({ scrollTop: 600 }, "slow");
+    });
 
     // validating the user id entered
     $scope.userIdChanged = function(){
@@ -162,7 +158,7 @@ merchantApp.controller('merchantCtrl', ['$scope', 'modalService', 'userFactory',
                                         localStorage.setItem("userloggedin", response.userId);
                                         localStorage.setItem("userloggedinName", response.name);
                                         localStorage.setItem("userloggedinAccess", response.access_token);
-                                        multiStepForm.nextStep();
+                                        $scope.user.signedUp = true;
                                     }
                                 },
                                 error: function() {
@@ -277,5 +273,127 @@ merchantApp.controller('merchantCtrl', ['$scope', 'modalService', 'userFactory',
         $scope.user.location = location;
         req.location = location;
     });
+    
+    $scope.payment = {
+        month: 0,
+        conversion: 500,
+        amount: 0,
+        promoCode: '',
+        discount: 0,
+        promoError: '',
+        checkingPromo: false,
+        validPromo: false,
+        paymentError: ''
+    };
+    
+    $scope.$watch('payment.month', function(){
+        $scope.payment.amount = $scope.payment.month * $scope.payment.conversion;
+    });
+    
+    $scope.validatePromoCode = function(){
+        
+        if($scope.payment.promoCode == '' || $scope.payment.promoCode == undefined){
+            $scope.payment.promoError = "Please enter a valid promo code!!";
+            return;
+        }
+        
+        $scope.payment.checkingPromo = true;
+        
+        var req = {
+            userId: userFactory.user,
+            promoCode: $scope.payment.promoCode,
+            accessToken: userFactory.userAccessToken
+        }
+        
+        $.ajax({
+            url: '/ValidatePromo',
+            type: 'post',
+            data: JSON.stringify(req),
+            contentType:"application/json",
+            dataType:"json",
+            success: function(response){
+                $scope.$apply(function(){
+                    $scope.payment.checkingPromo = false;
+                    $scope.payment.discount = 0;
+                    $scope.payment.validPromo = false;
+                    if(response.code == 0){
+                        $scope.payment.discount = response.discountAmount;
+                        $scope.payment.promoError = "Promo Applied: " + $scope.payment.promoCode;
+                        $scope.payment.validPromo = true;
+                    }else{
+                        if(response.code == 400)
+                            logoutService.logout();
+                        else{
+                            $scope.payment.discount = 0;
+                            $scope.payment.promoError = response.message;
+                        }
+                    }
+                });
+            },
+            error: function(){
+                $scope.payment.checkingPromo = false;
+                console.log("not able to add promo credit");
+            }
+        });
+    }
+    
+    $scope.removePromoCode = function(){
+        $scope.payment.promoError = '';
+        $scope.payment.promoCode = '';
+        $scope.payment.discount = 0;
+        $scope.payment.validPromo = false;
+    }
+    
+    $scope.completePayment = function(){
+        
+        var payableAmt = $scope.payment.amount - $scope.payment.discount;
+        
+        if(payableAmt > 0){
+            if(window.location.href.indexOf("frrndlease.com") > -1){
+                $scope.payment.paymentError = 'Currently we are not supporting payments!!';
+            }else{
+                var options = {
+                    key: "rzp_test_GwL1Gj4oI20Jeq",
+                    amount: payableAmt * 100,
+                    name: "Grey Labs LLP",
+                    description: "Paying Membership Fee",
+                    image: "images/fls-logo.png",
+                    prefill: {
+                        name: userFactory.userName,
+                        email: userFactory.user
+                    },
+                    handler: function (response){
+                        userFactory.payMembership($scope.payment.promoCode, payableAmt, response.razorpay_payment_id).then(function(res){
+                            if(res.data.code == 0)
+                                window.location.replace("myapp.html#/myprofile");
+                            else
+                                console.log(res);
+                        },
+                        function(error){});
+                    },
+                    modal: {
+                        ondismiss: function(){}
+                    }
+                }
+                var rzp1 = new Razorpay(options);
+                rzp1.open();
+            }
+        }else if(payableAmt == 0){
+            if($scope.payment.amount == $scope.payment.discount && $scope.payment.validPromo){
+                userFactory.payMembership($scope.payment.promoCode, 0, null).then(function(response){
+                    window.location.reload();
+                },
+                function(error){});
+            }else{
+                $scope.payment.paymentError = 'Please make the amount positive or apply a valid promo code';
+            }
+        }else if(payableAmt < 0){
+            if($scope.payment.validPromo)
+                $scope.payment.paymentError = 'Please make the amount positive or zero by adding more months';
+            else
+                $scope.payment.paymentError = 'Cannot pay negative amount. Please make it positive by adding more months';
+        }
+        
+    }
 
 }]);
