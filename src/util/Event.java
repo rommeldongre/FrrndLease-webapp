c87b5b425d3f44d5c0e8cfc6ee2a2a3bb962aa5b
AniruddhM <aniruddh@greylabs.org>
2017-01-23 12:13:56 +0530

    Resolving conflicts 2

--- src/util/Event.java
+++ src/util/Event.java
@@ -581,10 +581,10 @@ public class Event extends Connect{
 		String ACCOUNT_SID = "ACd0d67cc952f254e5cce8e6912527750a";
 		String AUTH_TOKEN = "69d7e761d7361e35fe43afd55acff5ff";
 		
-		/*if(ENV_CONFIG.equals("dev")){
+		if(ENV_CONFIG.equals("dev")){
 			LOGGER.warning("Sms Sent in dev environment");
 			return true;
-		}*/
+		}
 		
 		PreparedStatement ps = null;
 		ResultSet rs = null;
