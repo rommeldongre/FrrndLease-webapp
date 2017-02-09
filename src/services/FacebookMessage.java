1b4bf596ef2a5a253cacb9826108bc75fe51cc0e
AniruddhM <aniruddh@greylabs.org>
2017-02-09 17:33:07 +0530

    Changes to send cookies properly.

--- src/services/FacebookMessage.java
+++ src/services/FacebookMessage.java
@@ -1,33 +1,28 @@
 package services;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Enumeration;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.codehaus.jackson.map.ObjectMapper;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import pojos.AddLeadReqObj;
 import util.FlsBot;
-import util.FlsCredit;
+import util.FlsLogger;
 
 @WebServlet(description = "Get Message from Facebook Page", urlPatterns = { "/FacebookMessage" })
 public class FacebookMessage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private FlsLogger LOGGER = new FlsLogger(FacebookMessage.class.getName());
 	
 	private static final String access_token = "EAABiKoMD4QQBAEShbRZBBRZAg3bgWWR14SRsqZABqZBDhVpEh4Kgz2pHnLaS29iNuqKQMhLUhWbIibmKnzWsiVaamD5X1FluMvvNZC8eVJLZB4cgmq42abAPrZCl3mBuxd19LZArvPgyGdMGAzhkilRxeDmbZCT5R8u3x6HZCa20zTigZDZD";
 	private static final  String verify_token = "fb_bot";
@@ -36,15 +31,16 @@ public class FacebookMessage extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 			
+		LOGGER.info("Inside GET Method");
 		try{
 
 			String challenge = req.getParameter("hub.challenge");
 			hub_verify_token = req.getParameter("hub.verify_token");
 			
 			if (hub_verify_token.equals(verify_token)) {
-				System.out.println(challenge);
+				LOGGER.info(challenge);
 			}else{
-				System.out.println("Tokens not same");
+				LOGGER.info("Tokens not same");
 			}
 			
 			resp.setHeader("Expires", "0");
@@ -59,16 +55,17 @@ public class FacebookMessage extends HttpServlet {
 			resp.getWriter().flush();
 			resp.getWriter().close();
 		}catch(Exception e){
+			LOGGER.info("Exception occured inside GET method of Facebook Message");
 			e.printStackTrace();
-			System.out.println("Inside Exception of Facebook Message");
 		}
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		LOGGER.info("Inside POST Method");
 		FlsBot bot = new FlsBot();
-		String botResponse = null,botMessage=null;
+		String botMessage=null;
 		try{			  
 			
 			// Read from request
@@ -81,48 +78,23 @@ public class FacebookMessage extends HttpServlet {
 		    
 		    String data = buffer.toString();
 		    if(data.contains("postback")){
-		    	System.out.println("Post Back Message ");
 		    	botMessage = bot.postBackMessage(data);
 		    	
 		    }else{
 		    	JSONObject row = new JSONObject(data);
-			    System.out.println("JSON Object "+row);
 			    
 			    JSONArray rows = row.getJSONArray("entry");
 			    JSONObject first = rows.getJSONObject(0);
-		        System.out.println("entry array is"+rows);
-		        System.out.println("first array is "+first);
 		        
 		        JSONArray message_rows = first.getJSONArray("messaging");
 		        JSONObject first1 = message_rows.getJSONObject(0);
-		        System.out.println("message array is"+rows);
-		        System.out.println("first1 array is "+first1);
 		        JSONObject messageText = first1.getJSONObject("message");
-		        System.out.println("final message array is"+messageText);
-		        
-		        //JSONObject messageText2 = messageText.getJSONObject("text");
-		        System.out.println("final message array is"+messageText.getString("text"));
 		        
 		        JSONObject senderText = first1.getJSONObject("sender");
-		        System.out.println("final sender id is"+senderText.getString("id"));
 		        
 		        botMessage = bot.sendBotMessage(senderText.getString("id"), messageText.getString("text"),0);
 		         
-		       
-		        /*JSONObject root = new JSONObject();
-		        JSONObject c0 = new JSONObject();
-		        JSONObject c1 = new JSONObject();
-
-		        root.put("recipient", c0);
-		        root.put("message", c1);
-
-		        c0.put("id", senderText.getString("id"));
-		        c1.put("text", botMessage);
-		        botResponse= root.toString();
-		        System.out.println(root.toString());*/
 		    }
-		    
-		    System.out.println("\nJSON String 2 "+botMessage);
 		    
 			if(botMessage!=null){
 				HttpURLConnection httpcon = (HttpURLConnection) ((new URL("https://graph.facebook.com/v2.6/me/messages?access_token="+access_token).openConnection()));
@@ -137,14 +109,17 @@ public class FacebookMessage extends HttpServlet {
 				
 				int responseCode = httpcon.getResponseCode();
 				String responseMessage = httpcon.getResponseMessage();
-				System.out.println("Response Code for JSON POST : " + responseCode);
-				System.out.println("Response Message for JSON POST : " + responseMessage);
+				
+				LOGGER.info("Response Code for JSON POST : " + responseCode);
+				LOGGER.info("Response Message for JSON POST : " + responseMessage);
+					
 			}
 			
 
 		}catch(Exception e){
+			LOGGER.info("Exception occured inside POST method of Facebook Message");
 			e.printStackTrace();
-			System.out.println("Inside Exception of Facebook Message");
+			
 		}
 		
 	}
--- src/util/FlsBot.java
+++ src/util/FlsBot.java
@@ -1,10 +1,5 @@
 package util;
 
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,18 +8,14 @@ import java.sql.SQLException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import util.FlsCredit.Credit;
 import connect.Connect;
 
 public class FlsBot extends Connect{
 	
 	private FlsLogger LOGGER = new FlsLogger(FlsBot.class.getName());
 	
-	private static final String access_token = "EAABiKoMD4QQBAEShbRZBBRZAg3bgWWR14SRsqZABqZBDhVpEh4Kgz2pHnLaS29iNuqKQMhLUhWbIibmKnzWsiVaamD5X1FluMvvNZC8eVJLZB4cgmq42abAPrZCl3mBuxd19LZArvPgyGdMGAzhkilRxeDmbZCT5R8u3x6HZCa20zTigZDZD";
-	private String FB_URL = "https://graph.facebook.com/v2.6/me/";
-	
 	private String URL = FlsConfig.prefixUrl;
-	private String item_title=null,item_primary_image=null,item_desc=null,item_public_url=null;
+	private String item_title=null,item_public_url=null;
 	int item_offset=0;
 	
 	
@@ -34,7 +25,12 @@ public class FlsBot extends Connect{
 		LOGGER.info("Text from page is "+text);
 		boolean itemFound  = fetchItem(text,offset);
 		if(!itemFound){
-			String Message = "Sorry Please search for another item";
+			String Message =null;
+			if(offset==0){
+				 Message = "No Such Items. Please search for another item";
+			}else{
+				 Message = "No More Items. Please search for another item";
+			}
 			
 			JSONObject root = new JSONObject();
 	        JSONObject c0 = new JSONObject();
@@ -47,14 +43,13 @@ public class FlsBot extends Connect{
 	        c1.put("text", Message);
 	        botMessage= root.toString();
 		}else{
-			
-			 JSONObject root = new JSONObject();
+			   
+				JSONObject root = new JSONObject();
 		        JSONObject c0 = new JSONObject();
 		        JSONObject c1 = new JSONObject();
 
 		        JSONObject attachment = new JSONObject();
 		        JSONObject payload = new JSONObject();
-		       // JSONObject buttons = new JSONObject();
 		        JSONArray buttons = new JSONArray();
 		        JSONObject button1 = new JSONObject();
 		        JSONObject button2 = new JSONObject();
@@ -70,7 +65,7 @@ public class FlsBot extends Connect{
 		        attachment.put("payload", payload);
 
 		        payload.put("template_type", "button");
-		        payload.put("text", "An Item was found: "+item_title);
+		        payload.put("text", "An Item was found with title : "+item_title);
 		        payload.put("buttons", buttons);
 
 		        
@@ -88,91 +83,12 @@ public class FlsBot extends Connect{
 		        button3.put("url", URL);
 		        button3.put("title", "SignUp");
 		        buttons.put(button3);
-		        /*JSONObject root1 = new JSONObject();
-	        JSONObject c01 = new JSONObject();
-	        JSONObject c11 = new JSONObject();
-	        
-	        JSONObject attachment = new JSONObject();
-	        JSONObject payload = new JSONObject();
-	        JSONArray arrayButton= new JSONArray();
-	        JSONArray arrayelements= new JSONArray();
-	        JSONObject elementsObj = new JSONObject();
-	        JSONObject defaultAction = new JSONObject();
-	        
-	        JSONObject buttons1 = new JSONObject();
-	        JSONObject buttons2 = new JSONObject();
-
-	        root1.put("recipient", c01);
-	        	c01.put("id", userId);
-	        
-	        root1.put("message", c11);
-	        	c11.put("attachment", attachment);
-	        		attachment.put("type", "template");
-	        		attachment.put("payload", payload);
-	        			payload.put("template_type", "generic");
-	        			payload.put("elements", arrayelements);
-	        				arrayelements.put(elementsObj);
-	        					elementsObj.put("title", "Sample Title");
-	        					elementsObj.put("image_url", "https://s3-ap-southeast-1.amazonaws.com/fls-items-dev/sample-item-4-95/post/sample-item-4-95-primary-4495.png");
-	        					elementsObj.put("subtitle", "Sample Sub Title");
-	        					elementsObj.put("default_action", defaultAction);
-	        
-	        						defaultAction.put("type", "web_url");
-	        						defaultAction.put("url", "https://www.frrndlease.com/");
-	        						defaultAction.put("messenger_extensions", true);
-	        						defaultAction.put("webview_height_ratio", "tall");
-	        						defaultAction.put("fallback_url", "https://www.frrndlease.com/");
-	        
-	        					
-	        						
-	        						buttons1.put("type", "web_url");
-	        						buttons1.put("url", "https://google.com");
-	        						buttons1.put("title", "show website");
-	        					arrayButton.put(buttons1);
-
-	        						
-	        						buttons2.put("type", "postback");
-	        						buttons2.put("title", "Hi There");
-	        						buttons2.put("payload", "sample payload");
-	        					arrayButton.put(buttons2);
-	        					
-	        					elementsObj.put("buttons", arrayButton);*/
-	        
-	        String botMessage1= root.toString();
+		        
 	        botMessage= root.toString();
-	        //botMessage = "Sorry I could not find this item. Please try search for another item";
-	        LOGGER.info("JSON Output "+botMessage1);
 		}
 		
 		return botMessage;
 		
-	}
-	
-	public int addPersistentMenu(String payload, String type){
-		int responseCode= 0;
-		
-		LOGGER.info("Inside addPersistentMenu Method");
-		HttpURLConnection httpcon;
-		try {
-			httpcon = (HttpURLConnection) ((new URL(FB_URL+"messages?access_token="+access_token).openConnection()));
-			httpcon.setDoOutput(true);
-			httpcon.setRequestProperty("Content-Type", "application/json");
-			httpcon.setRequestMethod("POST");
-			httpcon.connect();
-			
-			final OutputStreamWriter osw = new OutputStreamWriter(httpcon.getOutputStream());
-			osw.write(payload);
-			osw.close();
-			
-			responseCode = httpcon.getResponseCode();
-			System.out.println("Response Code for JSON POST : " + responseCode);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return responseCode;
 	}
 	
 	
@@ -181,25 +97,15 @@ public class FlsBot extends Connect{
 		
 		LOGGER.info("Inside PostBAck Method");
 		JSONObject row = new JSONObject(postback);
-	    System.out.println("JSON Object "+row);
 	    
 	    JSONArray rows = row.getJSONArray("entry");
 	    JSONObject first = rows.getJSONObject(0);
-        System.out.println("entry array is"+rows);
-        System.out.println("first array is "+first);
         
         JSONArray message_rows = first.getJSONArray("messaging");
         JSONObject first1 = message_rows.getJSONObject(0);
-        System.out.println("message array is"+rows);
-        System.out.println("first1 array is "+first1);
         JSONObject messageText = first1.getJSONObject("postback");
-        System.out.println("final message array is"+messageText);
-        
-        //JSONObject messageText2 = messageText.getJSONObject("text");
-        System.out.println("final post back message array is"+messageText.getString("payload"));
         
         JSONObject senderText = first1.getJSONObject("sender");
-        System.out.println("final postback sender id is"+senderText.getString("id"));
         
         if(messageText.getString("payload").contains("=")){
         	String[] parts = messageText.getString("payload").split("=");
@@ -208,7 +114,6 @@ public class FlsBot extends Connect{
             
             int last_offset_int = Integer.parseInt(last_offset);
         
-            LOGGER.info("Inside PostBAck Method call"+ senderText.getString("id")+" "+last_text+" "+last_offset_int);
             PostbackMessage = sendBotMessage(senderText.getString("id"),last_text,last_offset_int);
         }else{
         	 switch (messageText.getString("payload")) {
@@ -233,7 +138,6 @@ public class FlsBot extends Connect{
              c0.put("id", senderText.getString("id"));
              c1.put("text", payloadMessage);
              PostbackMessage = root.toString();
-             //System.out.println(PostbackMessage);
         }
         
         return PostbackMessage;
@@ -243,17 +147,16 @@ public class FlsBot extends Connect{
 	public boolean fetchItem(String text,int offset){
 		boolean item_status = false;
 		
-		LOGGER.info("Inside logCredit Method");
+		LOGGER.info("Inside fetchItem Method");
 
 		Connection hcp = getConnectionFromPool();
 		PreparedStatement ps1 = null, ps2 = null;
-		ResultSet rs1=null, rs2=null;
+		ResultSet rs1=null;
 		
 		String item_uid= null;
 		try {
-			String sqlFetchItem = "SELECT * from `items` WHERE item_name LIKE '%"+text+"%' ORDER BY item_id DESC LIMIT "+offset+", 1";
+			String sqlFetchItem = "SELECT * from `items` WHERE item_status='InStore' AND item_name LIKE '%"+text+"%' ORDER BY item_id DESC LIMIT "+offset+", 1";
 			ps1 = hcp.prepareStatement(sqlFetchItem);
-			//ps1.setString(1, userId);
 
 			rs1 = ps1.executeQuery();
 
