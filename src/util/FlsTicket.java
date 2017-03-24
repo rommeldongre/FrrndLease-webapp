2ee1b9c065084e5aa8853d8536462f2453dbe80d
Ankit Karnany <ankit@greylabs.org>
2017-03-24 16:34:55 +0530

    Issue #189 - Add wishes to Signup Wizard

--- src/util/FlsTicket.java
+++ src/util/FlsTicket.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 import connect.Connect;
